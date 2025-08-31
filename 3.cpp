51ee0dc2b412bea795cd34f49cb5a0c4bdb826bf
Pradhi Rajak <95995251+pradhi02@users.noreply.github.com>
2025-08-31 15:06:20 +0530

    Add files via upload

--- 3.cpp
+++ 3.cpp
@@ -4,7 +4,11 @@
 #include<vector> 
 #include<list>
 #include<queue>
-
+#include<set>
+#include<iterator>
+#include<unordered_set>
+#include<map>
+#include<algorithm>
 
 using namespace std;  
 void explainPair(){
@@ -38,9 +42,15 @@ void expainVectors(){
 
     vec.push_back({1,2}); //if we want to insert a pair in the vector 
     vec.emplace_back(4,5); 
-    
+} 
 
 
+bool comp(pair<int,int>p1, pair<int,int>p2){
+    if(p1.second < p2.second) return true; 
+    if(p1.second > p2.second) return false; 
+    //when both the second are same 
+    if(p1.first > p2.first) return true; 
+    else return false;
 }
 int main(){
     cout<<"hello boy"; 
@@ -202,8 +212,124 @@ cout<<pq.top();
 pq.pop(); cout<<endl;
 cout<<pq.top();
 
+//maps in cpp 
+set<int>m; 
+m.insert(2);  //{2}
+m.insert(4); //{2,4}
+m.insert(6); //2,4,6
+m.insert(3);  //{2,3,4,6}
+m.insert(2);//only stores uniques data in theselves 
+//stores in a stored order 
+auto i=m.find(4); //return an iterator which points to this 4  
+
+int cnt=m.count(1);
+cout<<cnt; //1 existe, 0 not exist 
+
+//multiset 
+
+
+cout<<"MULTISET STARTS HERE";
+multiset<int>mu;  
+cout<<endl;
+mu.insert(12);
+mu.insert(13);
+mu.insert(12);
+mu.insert(19); 
+mu.insert(22);
+mu.insert(12);
+mu.insert(12); 
+//mu.erase(12); //erases all the orrcurances of 12 
+//mu.erase(mu.find(12),mu.find(12)+2);
+
+int cut=mu.count(12); //count gives the number of 12 in multiset 
+cout<<cut; //HERE WE WILL GET THE ANS 
+
+//mu.erase(mu.find(12)); 
+
+mu.erase(mu.find(12), std::next(mu.find(12), 2)); //erases two occurance of 12 in the multiset 
+cut=mu.count(12); //reinitializing the cut value;
+cout<<cut; 
+
+cout<<endl; 
+
+cout<<"UNORDERED SET STARTS HERE"; 
+
+unordered_set <int> un ={2,3,9,01,10};
+//constant time O(1);  
+
+cout<<endl;
+cout<<"map starts here";
+map<int, int>ma; //key and  alue associated withe the key 
+
+ma.insert({1,10}); 
+ma.insert({2,12});
+ma.insert({3,23}); 
+//ma.emplace({4,24}); .//sytrax error 
+ma.emplace(4,18);  
+
+for(auto it :ma){
+    cout<< it.first<<" "<<it.second<<endl;
+} 
+
+cout<<ma[1]; //this will print 10
+cout<<ma[5]; //this will give output 0 since no number is here
+cout<<endl;
+//algorithms we have to learn 
+
+int arr[]={34,48,21,1,23,3}; 
+
+//int n= arr.size();
+sort(arr,arr+6);
+for(int i=0;i<6;i++)
+{
+    cout<<arr[i]<<" "; } 
+
+cout<<endl;
+cout<<"new array function compatator";
+cout<<endl;
+
+    int arr2[]={34,23,84,19,383,983,12,1}; 
+    sort(arr2,arr2+8,greater<int>());
+    for(int i=0;i<8;i++)
+{
+    cout<<arr2[i]<<" "; }  
+    cout<<endl;
 
 
 
-    return 0; 
+
+
+    //pair usied and trying compare function 
+vector<pair<int,int>>a={{1,2},{3,4},{6,2},{4,6}}; 
+
+sort(a.begin(),a.end(),comp);
+
+for(auto it=a.begin() ; it!=a.end();it++){
+    cout<< it->first << it->second <<" ";
+} 
+
+
+int pra=18; 
+int pc=__builtin_popcount(pra);
+cout<<pc;
+cout<<endl;
+long long pradhi=273456789345678;
+long prad=__builtin_popcountll(pradhi); 
+cout<<prad;
+
+
+cout<<endl; 
+
+string s="2342"; 
+
+do{
+    cout<<s<<" ";
+} 
+while(next_permutation(s.begin(),s.end())); //printas all the permuatation of string 
+
+int maxi= *max_element(arr,arr+6);
+cout<<maxi;
+
+return 0;  
+
 }
